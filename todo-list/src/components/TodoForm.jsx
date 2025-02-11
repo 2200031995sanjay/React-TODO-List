51a6551d0c4857b123d935ce0d84aafeb00f2940
2200031995sanjay <2200031995@kluniversity.in>
2025-02-11 14:49:28 +0530

    TodoFrom Commit

---
+++ todo-list/src/components/TodoForm.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+
+const TodoForm = ({addTodo}) =>{
+    const [task, setTask] = useState("");
+
+    const handlesubmit=(e)=>{
+        e.preventDefault();
+        if(!task.trim()) return;
+        addTodo(task);
+        setTask("")
+
+    };
+
+    return(
+        <form onSubmit={handlesubmit} className='p-4 flex gap-2'>
+            <input 
+            type="text" 
+            value={task}
+            onChange={(e)=> setTask(e.target.value)}
+            placeholder='Add a task...'
+            className="border p-2 rounded w-full"
+            />
+            <button type='sumit' className="bg-blue-500 text-white px-4 py-2 rounded"> 
+                Add
+            </button>
+        </form>
+
+    );
+}
+
+export default TodoForm;
